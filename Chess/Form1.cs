2a8a8a4d1cfdc6f6502d7524dd15f0444b2159f0
Hihiz <public@mail.ru>
2022-08-01 14:06:11 +0300

    Вывод фигуры на кнопку

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -12,9 +12,21 @@ namespace Chess
 {
     public partial class Form1 : Form
     {
+        public Image chessSprites;
         public Form1()
         {
             InitializeComponent();
+
+            chessSprites = new Bitmap("D:\\Valera_Doks\\Desktop\\chess.png");
+            Image part = new Bitmap(50, 50);
+            Graphics g = Graphics.FromImage(part);
+            g.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0, 0, 150, 150, GraphicsUnit.Pixel);
+            button1.BackgroundImage = part;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
